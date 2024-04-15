21621bedf8b0bb84dc92983597f18b3809394a8b
4-110-3 <4-110-3@nje.local>
2024-04-15 11:19:51 +0200

    lab-07

---
+++ lab-07/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('DB_PASS','8Al@(oiw5z[0Buy8');
+    define('DB_NAME','manfred');
+
+    
+    /*
+    CREATE TABLE `manfred`.`users` (`id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(50) NOT NULL , `password` VARCHAR(200) NOT NULL , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+    */
